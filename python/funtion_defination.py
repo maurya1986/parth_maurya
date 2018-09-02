d5082e973ceea8b128cbc01028bf03648db38384
PARTHU MAURYA <pramod.nexgen@gmail.com>
2018-09-02 05:50:44 +0530

    This is the function defination programm

---
+++ python/funtion_defination.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Function definition is here
+
+def printinfo(str):
+    print str
+    return;
+
+# Now you can call printinfo function
+
+printinfo(str="PRAMOD KUMAR MAURYA")
